1e2278ecedc07dd77eddd34b218b1134cbe483c1
shtessel <79328777+shtessel@users.noreply.github.com>
2025-01-19 12:32:31 -0500

    Add files via upload

---
+++ SQL/datacamp/moto_sales/moto_sales.sql
@@ -0,0 +1,38 @@
+DROP TABLE moto_sales
+CREATE TABLE moto_sales
+
+(
+
+order_number 	VARCHAR,
+date DATE,
+warehouse 	VARCHAR (20),
+client_type VARCHAR (10),
+product_line VARCHAR (100),
+quantity INT,
+unit_price FLOAT,
+total FLOAT,
+payment VARCHAR (100),
+payment_fee FLOAT
+
+);
+
+---------------
+
+SELECT *
+FROM public.moto_sales;
+
+COPY public.moto_sales FROM 'C:/Users/shtes/Desktop/moto_sales.csv' DELIMITER ','
+CSV HEADER;
+
+SELECT 
+	product_line,
+    CASE WHEN DATE_PART('month', date) = 6 THEN 'June'
+         WHEN DATE_PART('month', date) = 7 THEN 'July'
+         WHEN DATE_PART('month', date) = 8 THEN 'August'
+    END as month,
+    warehouse,
+    ROUND(CAST(SUM(total * (1 - payment_fee)) AS NUMERIC), 2) AS net_revenue
+FROM public.moto_sales
+WHERE client_type = 'Wholesale'
+GROUP BY product_line, warehouse, month
+ORDER BY product_line, month, net_revenue DESC;
